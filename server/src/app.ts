b3a2c8b914ef7de445272b13e3f8db67e3f94ad1
ayoub-jouini <ayoubjouini135@gmail.com>
2023-02-28 00:15:22 +0100

    setting app.ts

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { Request, Response, NextFunction } from "express";
+
+import connect from "./configs/db";
+import vars from "./configs/vars";
+import HttpError from "./utils/HttpError";
+
+const app = express();
+
+app.use(express.json());
+
+//page not found error 404
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+//error handler
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+  if (!error) {
+    return;
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "an unknown error occurred!" });
+});
+
+connect();
+
+app.listen(vars.port, () =>
+  console.log(`server started on port ${vars.port} (${vars.url})`)
+);
