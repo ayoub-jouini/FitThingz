ca032231654058c2c994eb8f2d12bcc44d34dec2
ayoub-jouini <ayoubjouini135@gmail.com>
2023-02-28 02:09:42 +0100

    coach model

--- server/src/models/Coach.ts
+++ server/src/models/Coach.ts
@@ -1,85 +1,32 @@
 import { Schema, model } from "mongoose";
+
 import User from "./User";
-
-interface Identite {
-  type: string;
-  identite: string;
-  image: string;
-}
-
-interface Experience {
-  type: string;
-  experience: string;
-  image: string;
-}
-
-interface Commentaire {
-  userId: Schema.Types.ObjectId;
-  avatar: string;
-  date_pub: string;
-  commentaire: string;
-}
-
-interface Tarification {
-  titre: string;
-  description: string;
-  duree: number;
-  prix: number;
-  promo: number;
-}
+import Identite, { IIdentite } from "./Identite";
+import Experience, { IExperience } from "./Experience";
+import Tarification, { ITarification } from "./Tarification";
+import Commentaire, { ICommentaire } from "./Commentaire";
 
 interface ICoach {
   user: Schema.Types.ObjectId;
-  identite: Identite;
-  experience: Experience[];
+  identite: IIdentite;
+  experience: IExperience[];
   conn_aca: string;
-  tarification: Tarification[];
-  commentaire: Commentaire[];
+  tarification: ITarification[];
+  commentaire: ICommentaire[];
 }
 
 const coachSchema = new Schema<ICoach>({
   user: { type: Schema.Types.ObjectId, ref: User, required: true },
-  identite: {
-    type: new Schema<Identite>({
-      type: { type: String, required: true },
-      identite: { type: String, required: true },
-      image: { type: String, required: true },
-    }),
-    required: true,
-  },
+  identite: { type: Schema.Types.ObjectId, ref: Identite, required: true },
   experience: [
-    {
-      type: new Schema<Experience>({
-        type: { type: String, required: true },
-        experience: { type: String, required: true },
-        image: { type: String, required: true },
-      }),
-      required: true,
-    },
+    { type: Schema.Types.ObjectId, ref: Experience, required: true },
   ],
   conn_aca: { type: String, required: true },
   tarification: [
-    {
-      type: new Schema<Tarification>({
-        titre: { type: String, required: true },
-        description: { type: String, required: true },
-        duree: { type: Number, required: true },
-        prix: { type: Number, required: true },
-        promo: { type: Number, required: true },
-      }),
-      required: true,
-    },
+    { type: Schema.Types.ObjectId, ref: Tarification, required: true },
   ],
   commentaire: [
-    {
-      type: new Schema<Commentaire>({
-        userId: { type: Schema.Types.ObjectId, required: true },
-        avatar: { type: String, required: true },
-        date_pub: { type: String, required: true },
-        commentaire: { type: String, required: true },
-      }),
-      required: true,
-    },
+    { type: Schema.Types.ObjectId, ref: Commentaire, required: true },
   ],
 });
 
