41146f4036a7a3d028ff2b61482fea74d2ca3c36
ayoub-jouini <ayoubjouini135@gmail.com>
2023-04-06 01:21:34 +0100

    SignupModal

--- client/src/components/modals/SignupModal.tsx
+++ client/src/components/modals/SignupModal.tsx
@@ -15,45 +15,38 @@ const SignupModal: React.FC<Props> = (props) => {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ">
             <div className="relative w-screen h-screen">
               {/*content*/}
-              <div className="border-0 shadow-lg relative flex flex-col w-full h-screen bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+              <div
+                style={{ backgroundImage: "url(/images/backgroundsignup.png)" }}
+                className="border-0 bg-cover bg-tertiary shadow-lg relative flex flex-col w-full min-h-screen outline-none focus:outline-none "
+              >
+                <div className="my-10 mt-20">
+                  <div className="flex items-center justify-center">
+                    <p className="text-5xl text-textPrimary font-semibold">
+                      Welcome to
+                    </p>
+                    <img
+                      alt=""
+                      src="/icons/logo2.png"
+                      className="h-12 mx-5 -mb-3"
+                    />
+                  </div>
+                  <p className="text-center text-5xl text-textPrimary">
+                    I am...
                   </p>
                 </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
+                <div className="flex justify-center">
+                  <div className="border-2 border-primary bg-tertiary rounded-[81px] w-[440px] h-[440px] mx-10 flex flex-col justify-center items-center">
+                    <img alt="" src="/icons/coach.svg" className="h-32" />
+                    <p className="font-semibold text-3xl text-textPrimary">
+                      A Coach
+                    </p>
+                  </div>
+                  <div className="border-2 border-primary bg-tertiary rounded-[81px] w-[440px] h-[440px] mx-10 flex flex-col justify-center items-center">
+                    <img alt="" src="/icons/Sportif.svg" className="h-32" />
+                    <p className="font-semibold text-3xl text-textPrimary">
+                      An Athlete
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
