3139a07cd3b19acab9e4c2e15cf3b9a714224888
ayoub-jouini <ayoubjouini135@gmail.com>
2023-04-11 07:21:20 +0100

    searchBar

--- client/src/app/dashboard/coach/page.tsx
+++ client/src/app/dashboard/coach/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import CoachNavBar from "../../../components/nav/CoachNavBar";
 import { logout } from "../../../store/features/auth/authThunk";
 import { useDispatch } from "react-redux";
@@ -7,13 +8,19 @@ import { useDispatch } from "react-redux";
 export default function CoachDashboard() {
   const dispatch = useDispatch();
 
+  const [search, setSearch] = useState<string>("");
+
+  const handleSearch = (event: any) => {
+    setSearch(event.target.value);
+  };
+
   const handleLogout = () => {
     dispatch<any>(logout());
   };
 
   return (
     <main>
-      <CoachNavBar />
+      <CoachNavBar value={search} handleChange={handleSearch} />
       {/* <button onClick={handleLogout}>logout</button> */}
     </main>
   );
